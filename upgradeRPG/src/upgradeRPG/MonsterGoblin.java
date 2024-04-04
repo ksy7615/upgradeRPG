aaf63e6e24f3b86313ed8f2062ad3e1e83fc87b4
ksy7615 <sykim7615@daum.net>
2024-04-04 12:26:13 +0900

    Create Class MonsterGoblin extends UnitMonster

---
+++ upgradeRPG/src/upgradeRPG/MonsterGoblin.java
@@ -0,0 +1,21 @@
+package upgradeRPG;
+
+public class MonsterGoblin extends UnitMonster {
+
+	public MonsterGoblin() {
+		super("고블린", 800, 40);
+	}
+
+	@Override
+	public void defaultAttack(UnitPlayer player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void skill(UnitPlayer player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
