f9ca1d05a0075ab34100b5f392557fb6af3b1c12
ksy7615 <sykim7615@daum.net>
2024-04-04 16:23:26 +0900

    Delete variable static Of Class Item

--- upgradeRPG/src/upgradeRPG/Item.java
+++ upgradeRPG/src/upgradeRPG/Item.java
@@ -5,10 +5,10 @@ public class Item {
 	public static final int ARMOR = 2;
 	public static final int RING = 3;
 	
-	public static int kind; // 종류
-	public static String name; // 템명
-	public static int effect; // 효과
-	public static int price; // 가격
+	public int kind; // 종류
+	public String name; // 템명
+	public int effect; // 효과
+	public int price; // 가격
 	
 	public Item(int kind, String name, int effect, int price) {
 		this.kind = kind;
