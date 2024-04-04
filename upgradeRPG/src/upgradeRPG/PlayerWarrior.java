1b7e3183686c385a99577e5da888d9cff223a8f2
ksy7615 <sykim7615@daum.net>
2024-04-04 11:52:59 +0900

    Modify Constructor Of Class PlayerWarrior

--- upgradeRPG/src/upgradeRPG/PlayerWarrior.java
+++ upgradeRPG/src/upgradeRPG/PlayerWarrior.java
@@ -2,21 +2,21 @@ package upgradeRPG;
 
 public class PlayerWarrior extends UnitPlayer {
 
-	public PlayerWarrior(String name, int level, int hp, int mp, int power, int defence, int exp) {
-		super(name, level, hp, mp, power, defence, exp);
+	public PlayerWarrior() {
+		// 이름 레벨 체력 마나 공격력 방어력 경험치
+		super("전사", 1, 500, 100, 30, 30, 0);
 	}
 
 	@Override
 	public void defaultAttack(UnitMonster monster) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void skill(UnitMonster monster) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
 
 }
