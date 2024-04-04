c9cec876a534957854006a72a3259d0c13dcb828
ksy7615 <sykim7615@daum.net>
2024-04-04 18:25:41 +0900

    Add update method Of Class StageLobby

--- upgradeRPG/src/upgradeRPG/StageLobby.java
+++ upgradeRPG/src/upgradeRPG/StageLobby.java
@@ -4,8 +4,23 @@ public class StageLobby extends Stage {
 
 	@Override
 	public boolean update() {
-		// TODO Auto-generated method stub
-		return false;
+		System.out.println("╔══════════════════════╗");
+		System.out.println("          LOBBY        ");
+		System.out.println("         [1] 마을        ");
+		System.out.println("         [2] 전투        ");
+		System.out.println("         [0] 종료        ");
+	    System.out.println("╚══════════════════════╝");
+	    
+	    int select = GameManager.scanner.nextInt();
+	    
+	    if(select == 1)
+	    	GameManager.nextStage = "VILLAGE";
+	    else if(select == 2)
+	    	GameManager.nextStage = "BATTLE";
+	    else if(select == 3)
+	    	GameManager.nextStage = "";
+	    
+	    return false;
 	}
 
 	@Override
