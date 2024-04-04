1af431b726e14b4f809841dddc8108f7fe71bd12
ksy7615 <sykim7615@daum.net>
2024-04-04 14:02:30 +0900

    Delete overloading Constructor Of Class MonsterMimic

--- upgradeRPG/src/upgradeRPG/MonsterMimic.java
+++ upgradeRPG/src/upgradeRPG/MonsterMimic.java
@@ -1,14 +1,9 @@
 package upgradeRPG;
 
 public class MonsterMimic extends UnitMonster {
-
+	
 	public MonsterMimic() {
 		super("미믹", 650, 50);
-	}
-
-	// 확률로 오버로딩 생성자 호출하고 템 + 돈 + 경험치 드랍하기
-	public MonsterMimic(String name) {
-		super(name, 1, 0);
 	}
 
 	@Override
