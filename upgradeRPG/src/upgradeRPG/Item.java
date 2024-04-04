3daff431ad8531db0b7041edb35771e191f96902
ksy7615 <sykim7615@daum.net>
2024-04-04 15:07:52 +0900

    Modify variable kind (private > public) Of Class Item

--- upgradeRPG/src/upgradeRPG/Item.java
+++ upgradeRPG/src/upgradeRPG/Item.java
@@ -5,7 +5,7 @@ public class Item {
 	public static final int ARMOR = 2;
 	public static final int RING = 3;
 	
-	private int kind; // 종류
+	public int kind; // 종류
 	private String name; // 템명
 	private int effect; // 효과
 	private int price; // 가격
