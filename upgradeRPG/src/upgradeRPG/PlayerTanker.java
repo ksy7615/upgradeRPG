11e84746f4f14f0c69f208406cb4b42e8001df29
ksy7615 <sykim7615@daum.net>
2024-04-04 11:54:39 +0900

    Create Class PlayerTanker extends UnitPlayer

---
+++ upgradeRPG/src/upgradeRPG/PlayerTanker.java
@@ -0,0 +1,21 @@
+package upgradeRPG;
+
+public class PlayerTanker extends UnitPlayer {
+
+	public PlayerTanker() {
+		super("탱커", 1, 800, 100, 15, 50, 0);
+	}
+
+	@Override
+	public void defaultAttack(UnitMonster monster) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void skill(UnitMonster monster) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
