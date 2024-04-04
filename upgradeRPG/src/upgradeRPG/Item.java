c8b920fedcfd04e970620826d060584751d05c83
ksy7615 <sykim7615@daum.net>
2024-04-04 15:13:13 +0900

    Modify all variable public static Of Class Item

--- upgradeRPG/src/upgradeRPG/Item.java
+++ upgradeRPG/src/upgradeRPG/Item.java
@@ -5,10 +5,10 @@ public class Item {
 	public static final int ARMOR = 2;
 	public static final int RING = 3;
 	
-	public int kind; // 종류
-	private String name; // 템명
-	private int effect; // 효과
-	private int price; // 가격
+	public static int kind; // 종류
+	public static String name; // 템명
+	public static int effect; // 효과
+	public static int price; // 가격
 	
 	public Item(int kind, String name, int effect, int price) {
 		this.kind = kind;
