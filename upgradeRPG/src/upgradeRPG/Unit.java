4bd7a7bc1bd0a485fb76d764d10ac783e06e6cad
ksy7615 <sykim7615@daum.net>
2024-04-04 10:43:12 +0900

    Add variable Of Class Unit

--- upgradeRPG/src/upgradeRPG/Unit.java
+++ upgradeRPG/src/upgradeRPG/Unit.java
@@ -1,5 +1,18 @@
 package upgradeRPG;
 
 public class Unit {
+	private String name;
+	private int level;
+	
+//	private final int MAX_HP;
+	private int hp;
+//	private final int MAX_MP;
+	private int mp;
+	
+	private int power;
+	private int defence;
+	private int exp;
+	
+	private boolean party;
 
 }
