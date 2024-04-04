36e69cf4263b1fe58a72f4a59de631fe5eb11db0
ksy7615 <sykim7615@daum.net>
2024-04-04 18:02:26 +0900

    Add variable Of Class Guild

--- upgradeRPG/src/upgradeRPG/Guild.java
+++ upgradeRPG/src/upgradeRPG/Guild.java
@@ -1,5 +1,11 @@
 package upgradeRPG;
 
-public class Guild {
+import java.util.ArrayList;
+import java.util.Random;
 
+public class Guild {
+	private Random random = new Random();
+	private static final int PARTY_SIZE = 4;
+	private ArrayList<UnitPlayer> guildList = new ArrayList<>();
+	private UnitPlayer[] partyList;
 }
