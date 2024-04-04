697421182068fb9c0fcc8b30bd79b15ac9c8748a
ksy7615 <sykim7615@daum.net>
2024-04-04 14:38:37 +0900

    Add printMenu method Of Class Shop

--- upgradeRPG/src/upgradeRPG/Shop.java
+++ upgradeRPG/src/upgradeRPG/Shop.java
@@ -4,19 +4,28 @@ import java.util.ArrayList;
 
 public class Shop {
 	private ArrayList<Item> list = new ArrayList<>();
-	
+
 	public Shop() {
 		list.add(new Item(Item.WEAPON, "단검", 5, 1000));
 		list.add(new Item(Item.WEAPON, "소드", 10, 3000));
 		list.add(new Item(Item.WEAPON, "스피어", 15, 5000));
-		
+
 		list.add(new Item(Item.ARMOR, "투구", 7, 2000));
 		list.add(new Item(Item.ARMOR, "갑옷", 20, 10000));
 		list.add(new Item(Item.ARMOR, "망토", 5, 1500));
-		
+
 		list.add(new Item(Item.RING, "합금링", 8, 3500));
 		list.add(new Item(Item.RING, "실버링", 20, 10000));
 		list.add(new Item(Item.RING, "골드링", 35, 30000));
 	}
-	
+
+	public void printMenu() {
+		System.out.println("======= 상점 =======");
+		System.out.println("[1] 무기");
+		System.out.println("[2] 갑옷");
+		System.out.println("[3] 반지");
+		System.out.println("[0] 뒤로가기");
+		System.out.println("======= 상점 =======");
+	}
+
 }
