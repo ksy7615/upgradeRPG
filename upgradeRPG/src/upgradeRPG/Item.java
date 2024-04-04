3ccf677c182aae23263ca793b747b9f5c14e3f86
ksy7615 <sykim7615@daum.net>
2024-04-04 11:34:44 +0900

    Add Variable Of Class Item

--- upgradeRPG/src/upgradeRPG/Item.java
+++ upgradeRPG/src/upgradeRPG/Item.java
@@ -1,5 +1,12 @@
 package upgradeRPG;
 
 public class Item {
-
+	public static final int WEAPON = 1;
+	public static final int ARMOR = 2;
+	public static final int RING = 3;
+	
+	private int kind; // 종류
+	private String name; // 템명
+	private int effect; // 효과
+	private int price; // 가격
 }
