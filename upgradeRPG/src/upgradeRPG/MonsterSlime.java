c4ffe7a828fdd0f58d1d474ad9557c6960fd10b8
ksy7615 <sykim7615@daum.net>
2024-04-04 12:34:45 +0900

    Create Class MonsterSlime extends UnitMonster

---
+++ upgradeRPG/src/upgradeRPG/MonsterSlime.java
@@ -0,0 +1,22 @@
+package upgradeRPG;
+
+public class MonsterSlime extends UnitMonster {
+
+	public MonsterSlime() {
+		super("슬라임", 400, 30);
+	}
+
+	@Override
+	public void defaultAttack(UnitPlayer player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void skill(UnitPlayer player) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
