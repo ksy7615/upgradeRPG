df1d3e04a6a3a7e29111463739799634aff1c5e2
ksy7615 <sykim7615@daum.net>
2024-04-04 12:38:24 +0900

    Create Class MonsterBat extends UnitMonster

---
+++ upgradeRPG/src/upgradeRPG/MonsterBat.java
@@ -0,0 +1,21 @@
+package upgradeRPG;
+
+public class MonsterBat extends UnitMonster {
+
+	public MonsterBat(String name, int hp, int power) {
+		super("박쥐", 500, 50);
+	}
+
+	@Override
+	public void defaultAttack(UnitPlayer player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void skill(UnitPlayer player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
