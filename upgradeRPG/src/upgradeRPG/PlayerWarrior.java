85164f27cc2e4b084ed0f81032212da6091ac88a
ksy7615 <sykim7615@daum.net>
2024-04-04 11:50:00 +0900

    Create Class PlayerWarrior extends UnitPlayer

---
+++ upgradeRPG/src/upgradeRPG/PlayerWarrior.java
@@ -0,0 +1,22 @@
+package upgradeRPG;
+
+public class PlayerWarrior extends UnitPlayer {
+
+	public PlayerWarrior(String name, int level, int hp, int mp, int power, int defence, int exp) {
+		super(name, level, hp, mp, power, defence, exp);
+	}
+
+	@Override
+	public void defaultAttack(UnitMonster monster) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void skill(UnitMonster monster) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
