fcad139f569095a416bbfe4d0839d86c69cf61d5
ksy7615 <sykim7615@daum.net>
2024-04-04 15:24:52 +0900

    Add public static class(inventory, guild) Of Class UnitPlayer

--- upgradeRPG/src/upgradeRPG/UnitPlayer.java
+++ upgradeRPG/src/upgradeRPG/UnitPlayer.java
@@ -1,6 +1,9 @@
 package upgradeRPG;
 
 public abstract class UnitPlayer extends Unit {
+	public static int money;
+	public static Inventory inven = new Inventory();
+	public static Guild guild = new Guild();
 
 	public UnitPlayer(String name, int level, int hp, int mp, int power, int defence, int exp) {
 		super(name, level, hp, mp, power, defence, exp);
