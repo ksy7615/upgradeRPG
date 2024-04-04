40998ecdc692a7a26fa592add35f757fc85de1be
ksy7615 <sykim7615@daum.net>
2024-04-04 12:10:38 +0900

    Create Class PlayerWizard extends UnitPlayer

---
+++ upgradeRPG/src/upgradeRPG/PlayerWizard.java
@@ -0,0 +1,21 @@
+package upgradeRPG;
+
+public class PlayerWizard extends UnitPlayer {
+
+	public PlayerWizard() {
+		super("탱커", 1, 800, 100, 15, 50, 0);
+	}
+
+	@Override
+	public void defaultAttack(UnitMonster monster) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void skill(UnitMonster monster) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
