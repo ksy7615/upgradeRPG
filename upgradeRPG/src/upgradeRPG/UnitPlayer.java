c4f22c458a530c7fcda14f661c5cfd84728c7dc7
ksy7615 <sykim7615@daum.net>
2024-04-04 11:47:19 +0900

    Add abstract method for attack Of Class UnitPlayer

--- upgradeRPG/src/upgradeRPG/UnitPlayer.java
+++ upgradeRPG/src/upgradeRPG/UnitPlayer.java
@@ -1,9 +1,13 @@
 package upgradeRPG;
 
-public class UnitPlayer extends Unit {
+public abstract class UnitPlayer extends Unit {
 
 	public UnitPlayer(String name, int level, int hp, int mp, int power, int defence, int exp) {
 		super(name, level, hp, mp, power, defence, exp);
 	}
+	
+	public abstract void defaultAttack(UnitMonster monster);
+	
+	public abstract void skill(UnitMonster monster);
 
 }
