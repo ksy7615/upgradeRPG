826a0a7c61a7a7466bd2d28e8feb6c30dfb34ea2
ksy7615 <sykim7615@daum.net>
2024-04-04 10:59:45 +0900

    Add Constructor for Player Of Class Unit

--- upgradeRPG/src/upgradeRPG/Unit.java
+++ upgradeRPG/src/upgradeRPG/Unit.java
@@ -4,9 +4,9 @@ public class Unit {
 	private String name;
 	private int level;
 	
-//	private final int MAX_HP;
+	private int maxHp;
 	private int hp;
-//	private final int MAX_MP;
+	private int maxMp;
 	private int mp;
 	
 	private int power;
@@ -14,5 +14,17 @@ public class Unit {
 	private int exp;
 	
 	private boolean party;
+	
+	// 플레이어용
+	public Unit(String name, int level, int hp, int mp, int power, int defence, int exp) {
+		this.name = name;
+		this.level = level;
+		this.maxHp = hp;
+		this.hp = maxHp;
+		this.power = power;
+		this.defence = defence;
+		this.exp = exp;
+		this.party = false;
+	}
 
 }
