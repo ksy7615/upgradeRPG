618399fbb039d35c8e4939c307b699b547a4544c
ksy7615 <sykim7615@daum.net>
2024-04-04 18:33:27 +0900

    Add Constructor(overloading) Of Class UnitPlayer

--- upgradeRPG/src/upgradeRPG/UnitPlayer.java
+++ upgradeRPG/src/upgradeRPG/UnitPlayer.java
@@ -9,6 +9,10 @@ public abstract class UnitPlayer extends Unit {
 		super(name, level, hp, mp, power, defence, exp);
 	}
 	
+	public UnitPlayer(String name, int level, int hp, int mp, int power, int defence, int exp, boolean party) {
+		super(name, level, hp, mp, power, defence, exp, false);
+	}
+	
 	public abstract void defaultAttack(UnitMonster monster);
 	
 	public abstract void skill(UnitMonster monster);
