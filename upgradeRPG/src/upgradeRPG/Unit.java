9bd0233a38b4322aa22e07e47f4f878be273db75
ksy7615 <sykim7615@daum.net>
2024-04-04 11:03:57 +0900

    Add Constructor for Player(overloading) Of Class Unit

--- upgradeRPG/src/upgradeRPG/Unit.java
+++ upgradeRPG/src/upgradeRPG/Unit.java
@@ -26,5 +26,17 @@ public class Unit {
 		this.exp = exp;
 		this.party = false;
 	}
+	
+	// 플레이어용 생성자 오버로딩
+	public Unit(String name, int level, int hp, int mp, int power, int defence, int exp, boolean party) {
+		this.name = name;
+		this.level = level;
+		this.maxHp = hp;
+		this.hp = maxHp;
+		this.power = power;
+		this.defence = defence;
+		this.exp = exp;
+		this.party = party;
+	}
 
 }
