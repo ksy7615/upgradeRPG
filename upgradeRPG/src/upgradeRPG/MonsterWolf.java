d1fedb20387c2637e1b12b1ce0f78a820d5e6934
ksy7615 <sykim7615@daum.net>
2024-04-04 12:36:40 +0900

    Create Class MonsterWolf extends UnitMonster

---
+++ upgradeRPG/src/upgradeRPG/MonsterWolf.java
@@ -0,0 +1,21 @@
+package upgradeRPG;
+
+public class MonsterWolf extends UnitMonster {
+
+	public MonsterWolf() {
+		super("늑대", 500, 40);
+	}
+
+	@Override
+	public void defaultAttack(UnitPlayer player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void skill(UnitPlayer player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
