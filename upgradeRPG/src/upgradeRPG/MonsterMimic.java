146f01f7914bf8475c091c50333495d137a0925d
ksy7615 <sykim7615@daum.net>
2024-04-04 12:48:01 +0900

    Create Class MonsterMimic extends UnitMonster

---
+++ upgradeRPG/src/upgradeRPG/MonsterMimic.java
@@ -0,0 +1,21 @@
+package upgradeRPG;
+
+public class MonsterMimic extends UnitMonster {
+
+	public MonsterMimic() {
+		super("미믹", 650, 50);
+	}
+
+	@Override
+	public void defaultAttack(UnitPlayer player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void skill(UnitPlayer player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
