d11e5b72f32f226aa64f08e80918386fbe54b422
ksy7615 <sykim7615@daum.net>
2024-04-04 12:48:45 +0900

    Add constructor(overloading) Of Class MonsterMimic

--- upgradeRPG/src/upgradeRPG/MonsterMimic.java
+++ upgradeRPG/src/upgradeRPG/MonsterMimic.java
@@ -6,16 +6,21 @@ public class MonsterMimic extends UnitMonster {
 		super("미믹", 650, 50);
 	}
 
+	// 확률로 오버로딩 생성자 호출하고 템 + 돈 + 경험치 드랍하기
+	public MonsterMimic(String name) {
+		super(name, 1, 0);
+	}
+
 	@Override
 	public void defaultAttack(UnitPlayer player) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void skill(UnitPlayer player) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
