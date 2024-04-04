9dadc07358ae773b35f918c5a6e70f7da569e817
ksy7615 <sykim7615@daum.net>
2024-04-04 11:19:48 +0900

    Create Class UnitPlayer extends Unit

---
+++ upgradeRPG/src/upgradeRPG/UnitPlayer.java
@@ -0,0 +1,9 @@
+package upgradeRPG;
+
+public class UnitPlayer extends Unit {
+
+	public UnitPlayer(String name, int level, int hp, int mp, int power, int defence, int exp) {
+		super(name, level, hp, mp, power, defence, exp);
+	}
+
+}
