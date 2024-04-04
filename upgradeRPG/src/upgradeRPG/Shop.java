6002cc6d0857065f9848f1118ac91091af04e7d5
ksy7615 <sykim7615@daum.net>
2024-04-04 17:35:17 +0900

    Add buyRing Of Class Shop

--- upgradeRPG/src/upgradeRPG/Shop.java
+++ upgradeRPG/src/upgradeRPG/Shop.java
@@ -85,6 +85,16 @@ public class Shop {
 		}
 		System.out.println("==================");
 	}
+	
+	private void buyRing(int select) {
+		if (select < 1 || select > 3) {
+			return;
+		}
+
+		UnitPlayer.inven.inputItem(list.get(list.size() - 3 + select - 1));
+		UnitPlayer.money -= list.get(list.size() - 3 + select - 1).price;
+		System.out.println(list.get(list.size() - 3 + select - 1).name + "을(를) 구입했습니다.");
+	}
 
 	private void runMenu(int select) {
 		if (select == Item.WEAPON) {
@@ -96,7 +106,7 @@ public class Shop {
 		}
 		else if (select == Item.RING) {
 			printRing();
-//			buyRing();
+			buyRing(inputNumber("선택"));
 		}
 		else if (select == 0)
 			isRun = false;
