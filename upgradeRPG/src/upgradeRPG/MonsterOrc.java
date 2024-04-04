28fee59ac7c11441ec6c2a9adca7b6d333ecb895
ksy7615 <sykim7615@daum.net>
2024-04-04 12:29:54 +0900

    Create Class MonsterOrc extends UnitMonster

---
+++ upgradeRPG/src/upgradeRPG/MonsterOrc.java
@@ -0,0 +1,21 @@
+package upgradeRPG;
+
+public class MonsterOrc extends UnitMonster {
+
+	public MonsterOrc() {
+		super("오크", 1000, 100);
+	}
+
+	@Override
+	public void defaultAttack(UnitPlayer player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void skill(UnitPlayer player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
