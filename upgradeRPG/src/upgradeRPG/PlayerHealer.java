a1a1377b0bd9f3eac1926bd5d8211ac134bb2752
ksy7615 <sykim7615@daum.net>
2024-04-04 12:15:33 +0900

    Create Class PlayerHealer extends UnitPlayer

---
+++ upgradeRPG/src/upgradeRPG/PlayerHealer.java
@@ -0,0 +1,21 @@
+package upgradeRPG;
+
+public class PlayerHealer extends UnitPlayer {
+
+	public PlayerHealer() {
+		super("힐러", 1, 300, 200, 10, 20, 0);
+	}
+
+	@Override
+	public void defaultAttack(UnitMonster monster) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void skill(UnitMonster monster) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
