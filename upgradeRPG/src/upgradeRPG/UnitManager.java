f0d16785c675a0626203c7bc13e86576c14308aa
ksy7615 <sykim7615@daum.net>
2024-04-05 10:39:45 +0900

    Add variable (for setUnit) Of Class UnitManager

--- upgradeRPG/src/upgradeRPG/UnitManager.java
+++ upgradeRPG/src/upgradeRPG/UnitManager.java
@@ -1,15 +1,21 @@
 package upgradeRPG;
 
+import java.util.ArrayList;
+
 public class UnitManager {
-	
+	private ArrayList<UnitPlayer> playerList = new ArrayList<>();
+	private ArrayList<UnitMonster> monsterList = new ArrayList<>();
+	private String path = "upgradeRPG.";
+	private String[] monsters = { "MonsterBat", "MonsterGoblin", "MonsterOrc", "MonsterSlime", "MonsterWolf" };
+
 	private UnitManager() {
-		
+
 	}
-	
+
 	private static UnitManager instance = new UnitManager();
-	
+
 	public UnitManager getInstance() {
 		return instance;
 	}
-	
+
 }
