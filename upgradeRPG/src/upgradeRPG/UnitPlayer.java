16ff2afc28042fc5667aeb1e04486317cc349552
ksy7615 <sykim7615@daum.net>
2024-04-05 15:01:29 +0900

    Add Constructor Of Class UnitPlayer

--- upgradeRPG/src/upgradeRPG/UnitPlayer.java
+++ upgradeRPG/src/upgradeRPG/UnitPlayer.java
@@ -1,6 +1,7 @@
 package upgradeRPG;
 
 public abstract class UnitPlayer extends Unit {
+
 	private int level;
 	private int maxMp;
 	private int mp;
@@ -12,22 +13,21 @@ public abstract class UnitPlayer extends Unit {
 	public static int money;
 	public static Inventory inven = new Inventory();
 	public static Guild guild = new Guild();
+	
+	public UnitPlayer(String name, int level, int hp, int mp, int power, int defence, int exp) {
+		super(name, hp, power);
+		
+		this.level = level;
+		this.defence = defence;
+		this.exp = exp;
+		this.party = false;
+	}
 
 	public void init() {
 		money = 1000;
 		guild.setGuild();
 	}
+	
 
-	public UnitPlayer() {
-		
-	}
-
-	public UnitPlayer(String name, int level, int hp, int mp, int power, int defence, int exp) {
-		super(name, level, hp, mp, power, defence, exp);
-	}
-
-	public UnitPlayer(String name, int level, int hp, int mp, int power, int defence, int exp, boolean party) {
-		super(name, level, hp, mp, power, defence, exp, false);
-	}
 
 }
