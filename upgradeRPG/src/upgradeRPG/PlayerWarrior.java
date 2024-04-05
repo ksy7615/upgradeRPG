205729a0864ccf8ce647a93b0032995cd0402099
ksy7615 <sykim7615@daum.net>
2024-04-05 15:55:19 +0900

    Delete override method Of Class PlayerWarrior

--- upgradeRPG/src/upgradeRPG/PlayerWarrior.java
+++ upgradeRPG/src/upgradeRPG/PlayerWarrior.java
@@ -7,16 +7,4 @@ public class PlayerWarrior extends UnitPlayer {
 		super("전사", 1, 500, 100, 30, 30, 0);
 	}
 
-	@Override
-	public void defaultAttack(UnitMonster monster) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void skill(UnitMonster monster) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
