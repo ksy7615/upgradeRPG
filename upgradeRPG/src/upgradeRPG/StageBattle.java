14d3cbcb211ed7c91a11c845ccfd74854e46929e
ksy7615 <sykim7615@daum.net>
2024-04-05 12:23:31 +0900

    Add Variable Of Class StageBattle

--- upgradeRPG/src/upgradeRPG/StageBattle.java
+++ upgradeRPG/src/upgradeRPG/StageBattle.java
@@ -1,6 +1,13 @@
 package upgradeRPG;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class StageBattle extends Stage {
+	private Random random = new Random();
+	private ArrayList<UnitPlayer> playerList = new ArrayList<>();
+	private ArrayList<UnitMonster> monsterList = new ArrayList<>();
+	private UnitManager unitManager = UnitManager.getInstance();
 
 	@Override
 	public boolean update() {
