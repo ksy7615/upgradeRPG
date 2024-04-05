b87781115b0274d9cfb403e6a1f015f37c5542ad
ksy7615 <sykim7615@daum.net>
2024-04-05 16:13:45 +0900

    Delete abstract method Of Class MonsterSlime

--- upgradeRPG/src/upgradeRPG/MonsterSlime.java
+++ upgradeRPG/src/upgradeRPG/MonsterSlime.java
@@ -6,17 +6,4 @@ public class MonsterSlime extends UnitMonster {
 		super("슬라임", 400, 30);
 	}
 
-	@Override
-	public void defaultAttack(UnitPlayer player) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void skill(UnitPlayer player) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-
 }
