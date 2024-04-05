b7cf5e3f4bfe21895de4e4c888dfc53cd3f9c31c
ksy7615 <sykim7615@daum.net>
2024-04-05 14:29:47 +0900

    Delete Abstract method Of Class UnitMonster

--- upgradeRPG/src/upgradeRPG/UnitMonster.java
+++ upgradeRPG/src/upgradeRPG/UnitMonster.java
@@ -5,9 +5,5 @@ public abstract class UnitMonster extends Unit {
 	public UnitMonster(String name, int hp, int power) {
 		super(name, hp, power);
 	}
-	
-	public abstract void defaultAttack(UnitPlayer player);
-	
-	public abstract void skill(UnitPlayer player);
 
 }
