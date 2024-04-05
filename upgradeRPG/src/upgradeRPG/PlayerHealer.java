ee3c9957a44ba06f5be1031b21ce3a108d792107
ksy7615 <sykim7615@daum.net>
2024-04-05 15:23:17 +0900

    Add skill method Of Class PlayerHealer

--- upgradeRPG/src/upgradeRPG/PlayerHealer.java
+++ upgradeRPG/src/upgradeRPG/PlayerHealer.java
@@ -5,17 +5,15 @@ public class PlayerHealer extends UnitPlayer {
 	public PlayerHealer() {
 		super("힐러", 1, 300, 200, 10, 20, 0);
 	}
+	
+	public void skill(UnitPlayer player) {
+		System.out.println(String.format("[%s]의 스킬 [회복]\n", this.getName()));
+		int pray = 100;
 
-	@Override
-	public void defaultAttack(UnitMonster monster) {
-		// TODO Auto-generated method stub
-		
-	}
+		player.setHp(player.getHp() + pray);
+		this.setMp(this.getMp() - 50);
 
-	@Override
-	public void skill(UnitMonster monster) {
-		// TODO Auto-generated method stub
-		
+		System.out.println(String.format("[%s]가 모두를 [%d]만큼 회복 시켰습니다!\n", this.getName(), pray));
 	}
 
 }
