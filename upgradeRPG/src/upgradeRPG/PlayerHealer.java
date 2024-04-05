086f88868521e9da29edcc00409fe9bf64c023dc
ksy7615 <sykim7615@daum.net>
2024-04-05 15:34:48 +0900

    Modify skill Of Class PlayerHealer

--- upgradeRPG/src/upgradeRPG/PlayerHealer.java
+++ upgradeRPG/src/upgradeRPG/PlayerHealer.java
@@ -1,19 +1,24 @@
 package upgradeRPG;
 
+import java.util.ArrayList;
+
 public class PlayerHealer extends UnitPlayer {
 
 	public PlayerHealer() {
 		super("힐러", 1, 300, 200, 10, 20, 0);
 	}
-	
-	public void skill(UnitPlayer player) {
+
+	public void skill(ArrayList<UnitPlayer> player) {
 		System.out.println(String.format("[%s]의 스킬 [회복]\n", this.getName()));
-		int pray = 100;
-
-		player.setHp(player.getHp() + pray);
+		int recovery = 100;
+		
+		// 파티원 모두 회복 시켜주기 >> 이렇게 돌리면 다 .. 회복 들어가나 *돌려보고 수정하기
+		for (int i = 0; i<Guild.PARTY_SIZE; i++) {
+			player.get(i).setHp(player.get(i).getHp() + recovery);
+		}
+		
 		this.setMp(this.getMp() - 50);
-
-		System.out.println(String.format("[%s]가 모두를 [%d]만큼 회복 시켰습니다!\n", this.getName(), pray));
+		System.out.println(String.format("[%s]가 모두를 [%d]만큼 회복 시켰습니다!\n", this.getName(), recovery));
 	}
 
 }
