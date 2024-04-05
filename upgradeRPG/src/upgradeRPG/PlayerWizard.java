de9651d21d354b480ef23a4856ffda945aab9556
ksy7615 <sykim7615@daum.net>
2024-04-05 15:56:41 +0900

    Delete override method Of Class PlayerWizard

--- upgradeRPG/src/upgradeRPG/PlayerWizard.java
+++ upgradeRPG/src/upgradeRPG/PlayerWizard.java
@@ -5,17 +5,5 @@ public class PlayerWizard extends UnitPlayer {
 	public PlayerWizard() {
 		super("마법사", 1, 300, 200, 30, 15, 0);
 	}
-
-	@Override
-	public void defaultAttack(UnitMonster monster) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void skill(UnitMonster monster) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
 }
