0dd8e62a81737d9a6ba48ad4942d8ce0328c7694
ksy7615 <sykim7615@daum.net>
2024-04-05 14:35:01 +0900

    Modify variable location(by unit) Of Class UnitPlayer

--- upgradeRPG/src/upgradeRPG/UnitPlayer.java
+++ upgradeRPG/src/upgradeRPG/UnitPlayer.java
@@ -1,25 +1,33 @@
 package upgradeRPG;
 
 public abstract class UnitPlayer extends Unit {
+	private int level;
+	private int maxMp;
+	private int mp;
+	private int defence;
+	private int exp;
+
+	private boolean party;
+
 	public static int money;
 	public static Inventory inven = new Inventory();
 	public static Guild guild = new Guild();
-	
+
 	public void init() {
 		money = 1000;
 		guild.setGuild();
 	}
-	
+
 	public UnitPlayer() {
-		super();
+		
 	}
 
 	public UnitPlayer(String name, int level, int hp, int mp, int power, int defence, int exp) {
 		super(name, level, hp, mp, power, defence, exp);
 	}
-	
+
 	public UnitPlayer(String name, int level, int hp, int mp, int power, int defence, int exp, boolean party) {
 		super(name, level, hp, mp, power, defence, exp, false);
 	}
-	
+
 }
