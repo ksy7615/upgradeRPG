31ffbff69ee51db88abee991b667a445256d02a0
ksy7615 <sykim7615@daum.net>
2024-04-05 11:49:41 +0900

    Create Class StageBattle extends Stage

---
+++ upgradeRPG/src/upgradeRPG/StageBattle.java
@@ -0,0 +1,17 @@
+package upgradeRPG;
+
+public class StageBattle extends Stage {
+
+	@Override
+	public boolean update() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
