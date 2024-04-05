1cc3dbb9e3db54023177564da06250fcbef40083
ksy7615 <sykim7615@daum.net>
2024-04-05 15:58:41 +0900

    Modify Class UnitMonster (Delete abstract)

--- upgradeRPG/src/upgradeRPG/UnitMonster.java
+++ upgradeRPG/src/upgradeRPG/UnitMonster.java
@@ -1,9 +1,20 @@
 package upgradeRPG;
 
-public abstract class UnitMonster extends Unit {
+public class UnitMonster extends Unit {
 
 	public UnitMonster(String name, int hp, int power) {
 		super(name, hp, power);
+	}
+
+	@Override
+	public void defaultAttack(Unit unit) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void skill(Unit unit) {
+		
 	}
 
 }
