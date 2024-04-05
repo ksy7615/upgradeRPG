4e4cecd8759f7ccab73cbd1aaa4aaaaa644bdd3f
ksy7615 <sykim7615@daum.net>
2024-04-05 12:16:29 +0900

    Add update method in print Of Class StageVillage

--- upgradeRPG/src/upgradeRPG/StageVillage.java
+++ upgradeRPG/src/upgradeRPG/StageVillage.java
@@ -4,14 +4,39 @@ public class StageVillage extends Stage {
 
 	@Override
 	public boolean update() {
-		// TODO Auto-generated method stub
+		System.out.println("╔══════════════════════╗");
+		System.out.println("         VILLAGE        ");
+		System.out.println("        [1] 길드         ");
+		System.out.println("        [2] 상점         ");
+		System.out.println("        [3] 인벤토리       ");
+		System.out.println("        [4] 저장         ");
+		System.out.println("        [5] 로드         ");
+		System.out.println("        [0] 뒤로가기       ");
+		System.out.println("╚══════════════════════╝");
+
+		int select = GameManager.scanner.nextInt();
+
+		if (select == 1) {
+
+		} else if (select == 2) {
+
+		} else if (select == 3) {
+
+		} else if (select == 4) {
+
+		} else if (select == 5) {
+
+		} else if (select == 0) {
+			GameManager.nextStage = "";
+		}
+
 		return false;
 	}
 
 	@Override
 	public void init() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
