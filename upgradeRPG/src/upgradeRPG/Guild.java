08ccda8a61d749238f26abacad20d3c26dbc2679
ksy7615 <sykim7615@daum.net>
2024-04-05 15:24:44 +0900

    Modify final int(private -> public) Of Class Guild

--- upgradeRPG/src/upgradeRPG/Guild.java
+++ upgradeRPG/src/upgradeRPG/Guild.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public class Guild {
 	private Random random = new Random();
-	private static final int PARTY_SIZE = 4;
+	public static final int PARTY_SIZE = 4;
 	private ArrayList<UnitPlayer> guildList = new ArrayList<>();
 	private UnitPlayer[] partyList;
 	
