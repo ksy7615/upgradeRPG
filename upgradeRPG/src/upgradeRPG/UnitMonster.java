522df81f205a2f0283df968670ebdf98b582343b
ksy7615 <sykim7615@daum.net>
2024-04-05 16:11:04 +0900

    Add defaultAttack Of Class UnitMonster

--- upgradeRPG/src/upgradeRPG/UnitMonster.java
+++ upgradeRPG/src/upgradeRPG/UnitMonster.java
@@ -7,14 +7,32 @@ public class UnitMonster extends Unit {
 	}
 
 	@Override
-	public void defaultAttack(Unit unit) {
-		// TODO Auto-generated method stub
-		
+	public void defaultAttack(Unit player) {
+		// 플레이어용이라 공백
 	}
 
 	@Override
 	public void skill(Unit unit) {
+		// 스킬 각자 다르게 부여
+	}
+
+	@Override
+	public void defaultAttack(UnitPlayer player) {
+		System.out.println(String.format("[%s]의 공격\n", this.getName()));
+
+		int hit = this.getPower() - player.getDefence() / 5;
+
+		if (hit <= 0) {
+			hit = 0;
+			System.out.println("MISS");
+			return;
+		}
 		
+		player.setHp(player.getHp() - hit);
+
+		if (player.getHp() <= 0) {
+			player.setHp(0);
+		}
 	}
 
 }
