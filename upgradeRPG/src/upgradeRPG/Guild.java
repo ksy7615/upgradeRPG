ac8dd6bea49835f962f462de9f26b00d5e837db0
ksy7615 <sykim7615@daum.net>
2024-04-05 16:25:13 +0900

    Add setGuild method Of Class Guild

--- upgradeRPG/src/upgradeRPG/Guild.java
+++ upgradeRPG/src/upgradeRPG/Guild.java
@@ -8,16 +8,29 @@ public class Guild {
 	public static final int PARTY_SIZE = 4;
 	private ArrayList<UnitPlayer> guildList = new ArrayList<>();
 	private UnitPlayer[] partyList;
-	
+
 	public void setGuild() {
 		PlayerWarrior warrior = new PlayerWarrior();
 		PlayerTanker tanker = new PlayerTanker();
 		PlayerWizard wizard = new PlayerWizard();
 		PlayerHealer healer = new PlayerHealer();
-		
+
 		guildList.add(warrior);
 		guildList.add(tanker);
 		guildList.add(wizard);
 		guildList.add(healer);
+
+		for (int i = 0; i < PARTY_SIZE; i++) {
+			guildList.get(i).setParty();
+		}
+
+		partyList = new UnitPlayer[PARTY_SIZE];
+
+		int index = 0;
+		for (int i = 0; i < guildList.size(); i++) {
+			if(guildList.get(i).isParty()) {
+				partyList[index++] = guildList.get(i);
+			}
+		}
 	}
 }
