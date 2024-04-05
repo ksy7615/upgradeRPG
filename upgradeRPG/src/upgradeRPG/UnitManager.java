a2ddf297cbeb22655991417a55ab2dcc208bd1ec
ksy7615 <sykim7615@daum.net>
2024-04-05 10:30:36 +0900

    Add Singleton Pattern Of Class UnitManager

--- upgradeRPG/src/upgradeRPG/UnitManager.java
+++ upgradeRPG/src/upgradeRPG/UnitManager.java
@@ -2,4 +2,14 @@ package upgradeRPG;
 
 public class UnitManager {
 	
+	private UnitManager() {
+		
+	}
+	
+	private static UnitManager instance = new UnitManager();
+	
+	public UnitManager getInstance() {
+		return instance;
+	}
+	
 }
