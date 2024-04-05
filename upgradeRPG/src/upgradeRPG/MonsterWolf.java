7ef462220738ee68175b2bf16c9db4d52944ec1f
ksy7615 <sykim7615@daum.net>
2024-04-05 16:13:31 +0900

    Delete abstract method Of Class MonsterWolf

--- upgradeRPG/src/upgradeRPG/MonsterWolf.java
+++ upgradeRPG/src/upgradeRPG/MonsterWolf.java
@@ -6,16 +6,4 @@ public class MonsterWolf extends UnitMonster {
 		super("늑대", 500, 40);
 	}
 
-	@Override
-	public void defaultAttack(UnitPlayer player) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void skill(UnitPlayer player) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
