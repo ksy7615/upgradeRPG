331a0cf10b7a81e95f74aaa4335458f49525163e
ksy7615 <sykim7615@daum.net>
2024-04-08 10:26:37 +0900

    Add printInventoryMenu Of Class Inventory

--- upgradeRPG/src/upgradeRPG/Inventory.java
+++ upgradeRPG/src/upgradeRPG/Inventory.java
@@ -5,6 +5,14 @@ import java.util.ArrayList;
 public class Inventory {
 	public static ArrayList<Item> itemList = new ArrayList<>();
 	
+	public void printInventoryMenu() {
+		System.out.println("======= 인벤토리 =======");
+		System.out.println("[1] 장비착용");
+		System.out.println("[2] 아이템 판매");
+		System.out.println("[0] 뒤로가기");
+		System.out.println("=====================");
+	}
+	
 	public void inputItem(Item item) {
 		itemList.add(item);
 	}
