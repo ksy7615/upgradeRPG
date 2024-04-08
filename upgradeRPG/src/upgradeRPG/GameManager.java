b464c8d2206e2681804c41ba865c9f5e6909983d
ksy7615 <sykim7615@daum.net>
2024-04-08 16:39:02 +0900

    Add Constructor(Singleton) Of Class GameManager

--- upgradeRPG/src/upgradeRPG/GameManager.java
+++ upgradeRPG/src/upgradeRPG/GameManager.java
@@ -1,9 +1,30 @@
 package upgradeRPG;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class GameManager {
 	public static Scanner scanner = new Scanner(System.in);
+	private Map<String, Stage> stageList = new HashMap<String, Stage>();
 	public static String nextStage = "";
+	private String curStage = "";
+
+	private GameManager() {
+		stageList.put("LOBBY", new StageLobby());
+		stageList.put("VILLAGE", new StageVillage());
+		stageList.put("BATTLE", new StageBattle());
+		stageList.put("REWARD", new StageReward());
+		stageList.put("MYSTERY", new StageMystery());
+		stageList.put("BIGREWARD", new StageBigReward());
+		stageList.put("MIMIC", new StageMimic());
+
+		nextStage = "LOBBY";
+	}
 	
+	private static GameManager instance = new GameManager();
+	
+	public static GameManager getInstance() {
+		return instance;
+	}
 }
