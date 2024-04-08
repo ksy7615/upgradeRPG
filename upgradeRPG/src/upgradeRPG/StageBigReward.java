4ac873fa2ba7b967f879d822bf0fdb29de676be4
ksy7615 <sykim7615@daum.net>
2024-04-08 15:10:09 +0900

    Create Class StageBigReward extends Stage

---
+++ upgradeRPG/src/upgradeRPG/StageBigReward.java
@@ -0,0 +1,17 @@
+package upgradeRPG;
+
+public class StageBigReward extends Stage {
+
+	@Override
+	public boolean update() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
