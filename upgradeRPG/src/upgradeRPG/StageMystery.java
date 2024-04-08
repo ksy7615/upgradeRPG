2a6889c037b46dd553b9d1de5dcd6a6565b98e7f
ksy7615 <sykim7615@daum.net>
2024-04-08 15:06:41 +0900

    Create Class StageMystery extends Stage

---
+++ upgradeRPG/src/upgradeRPG/StageMystery.java
@@ -0,0 +1,17 @@
+package upgradeRPG;
+
+public class StageMystery extends Stage {
+
+	@Override
+	public boolean update() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
