5755a90aff009a4d80eb628751177c313a99f80b
ksy7615 <sykim7615@daum.net>
2024-04-09 12:49:52 +0900

    Add save method( default setting Player) Of Class FileData

--- upgradeRPG/src/upgradeRPG/FileData.java
+++ upgradeRPG/src/upgradeRPG/FileData.java
@@ -4,10 +4,27 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class FileData {
 	File file = null;
 	FileWriter fileWriter = null;
 	FileReader fileReader = null;
 	BufferedReader bufferedReader = null;
+	
+	String path = "gameData.txt";
+	
+	public void save() throws IOException {
+		// (1) 플레이어 유닛 기본 세팅 저장
+		UnitManager unitManager = UnitManager.getInstance();
+	
+		fileWriter = new FileWriter(path);
+		ArrayList<UnitPlayer> temp = unitManager.player.guild.guildList;
+		
+		String gameData = "";
+	
+		gameData += UnitPlayer.money + "\r\n" + temp.size() + "\r\n";
+	}
+	
 }
