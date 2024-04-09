56c7b830b82b39b753b77cd440f9558b2d2baa05
ksy7615 <sykim7615@daum.net>
2024-04-09 12:40:12 +0900

    Add variable for file function Of Class FileData

--- upgradeRPG/src/upgradeRPG/FileData.java
+++ upgradeRPG/src/upgradeRPG/FileData.java
@@ -1,5 +1,13 @@
 package upgradeRPG;
 
-public class FileData {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 
+public class FileData {
+	File file = null;
+	FileWriter fileWriter = null;
+	FileReader fileReader = null;
+	BufferedReader bufferedReader = null;
 }
