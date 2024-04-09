b6dbbac69957a4bf6a26c07d37f82e7497680667
ksy7615 <sykim7615@daum.net>
2024-04-09 11:19:25 +0900

    Add update Method Of Class StageReward

--- upgradeRPG/src/upgradeRPG/StageReward.java
+++ upgradeRPG/src/upgradeRPG/StageReward.java
@@ -1,10 +1,19 @@
 package upgradeRPG;
 
+import java.util.Random;
+
 public class StageReward extends Stage {
+	private UnitPlayer player = new UnitPlayer();
+	private Random random = new Random();
 
 	@Override
 	public boolean update() {
-		// TODO Auto-generated method stub
+		int ranMoney = random.nextInt(4000)+1000;
+		
+		System.out.println(String.format("🪙 %d원을 획득했습니다.", ranMoney));
+		player.money += ranMoney;
+		
+		GameManager.nextStage = "LOBBY";
 		return false;
 	}
 
